da35dae51594ee4a80abea86672fa221e7b4f68b
Fabian Yate <fabianyate31@gmail.com>
2024-09-21 23:29:17 -0500

    rename feign class name

--- src/main/java/com/emazon/cartservice/adapter/driven/feign/client/IStockFeignClient.java
+++ src/main/java/com/emazon/cartservice/adapter/driven/feign/client/IStockFeignClient.java
@@ -1,8 +1,8 @@
 package com.emazon.cartservice.adapter.driven.feign.client;
 
 import com.emazon.cartservice.adapter.driven.feign.dto.CategoryNameResponse;
-import com.emazon.cartservice.adapter.driven.feign.util.StockFeignClientApiPathConstants;
-import com.emazon.cartservice.adapter.driven.feign.util.StockFeignClientConstants;
+import com.emazon.cartservice.adapter.driven.feign.util.FeignClientApiPathConstants;
+import com.emazon.cartservice.adapter.driven.feign.util.FeignClientConstants;
 import com.emazon.cartservice.configuration.feign.FeignInterceptor;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,20 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 
 @FeignClient(
-        name = StockFeignClientConstants.STOCK_SERVICE_NAME,
+        name = FeignClientConstants.STOCK_SERVICE_NAME,
         url = "${stock.service.url}",
         configuration = FeignInterceptor.class)
 public interface IStockFeignClient {
 
-    @GetMapping(value = StockFeignClientApiPathConstants.EXISTS_BY_ID)
+    @GetMapping(value = FeignClientApiPathConstants.EXISTS_BY_ID)
     boolean existsById(@PathVariable Long articleId);
 
-    @GetMapping(value = StockFeignClientApiPathConstants.AVAILABLE_STOCK)
+    @GetMapping(value = FeignClientApiPathConstants.AVAILABLE_STOCK)
     boolean isStockAvailable(
-            @PathVariable(StockFeignClientApiPathConstants.ARTICLE_ID_REQUEST_PARAM) Long articleId,
-            @RequestParam(StockFeignClientApiPathConstants.QUANTITY_REQUEST_PARAM) Integer quantity);
+            @PathVariable(FeignClientApiPathConstants.ARTICLE_ID_REQUEST_PARAM) Long articleId,
+            @RequestParam(FeignClientApiPathConstants.QUANTITY_REQUEST_PARAM) Integer quantity);
 
-    @GetMapping(value = StockFeignClientApiPathConstants.GET_CATEGORIES_NAME)
+    @GetMapping(value = FeignClientApiPathConstants.GET_CATEGORIES_NAME)
     List<CategoryNameResponse> getCategoriesNameByArticleId(
-            @PathVariable(StockFeignClientApiPathConstants.ARTICLE_ID_REQUEST_PARAM) Long articleId);
+            @PathVariable(FeignClientApiPathConstants.ARTICLE_ID_REQUEST_PARAM) Long articleId);
 }
