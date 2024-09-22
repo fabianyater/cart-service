df09b6ac272de06d2d945e2c21f64ef9909a31cb
Fabian Yate <fabianyate31@gmail.com>
2024-09-21 23:29:59 -0500

    add role permissions constants

---
+++ src/main/java/com/emazon/cartservice/adapter/driving/http/util/RoleConstants.java
@@ -0,0 +1,9 @@
+package com.emazon.cartservice.adapter.driving.http.util;
+
+public class RoleConstants {
+    private RoleConstants() {
+    }
+
+    public static final String HAS_ROLE_CUSTOMER = "hasRole('CUSTOMER')";
+
+}
